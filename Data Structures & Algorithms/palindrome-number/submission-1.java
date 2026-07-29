9728c61fcfc30de4fbeaebd3e47935fc011c258b
Anupam Singh <anupamsngh518@gmail.com>
2026-07-29 11:06:44 +0200

    Add: palindrome-number - submission-1

---
+++ Data Structures & Algorithms/palindrome-number/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        List<Integer> list = new ArrayList<>();
+
+        int temp = x;
+        while (temp != 0) {
+            list.add(temp % 10);
+            temp = temp / 10;
+        }
+
+        if (list.size() == 1 && list.get(0) > 0) return true;
+        if (list.size() == 1 && list.get(0) < 0) return false;
+
+        int l = 0;
+        int r = list.size() - 1;
+
+        while (l < r) {
+            if (list.get(l) != list.get(r)) return false;
+            l++;
+            r--;
+        }
+
+        return true;
+        
+    }
+}
