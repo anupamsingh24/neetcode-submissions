28f3ed5ee67a3a9fa3c819c63e2e4a983b92fe9c
Anupam Singh <anupamsngh518@gmail.com>
2026-08-29 18:01:55 +0200

    Add: set-mismatch - submission-2

---
+++ Data Structures & Algorithms/set-mismatch/submission-2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] copy = nums.clone();
+        Arrays.sort(copy);
+
+        for (int i = 0 ; i < nums.length; i++) {
+            if (i + 1 != copy[i]) return new int[]{copy[i], i+1};
+        }
+        return new int[]{-1};
+    }
+}
